8ad68f9ae3251814c3ab54101e7613a74b058c52
Derek Araujo <derekaraujo19@gmail.com>
2022-02-23 01:47:10 -0500

    commit tree 7cf73a63803b6e06cd9d3d88dedb63a73cb8bd46

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,22 @@
 # Write your code here.
+require 'pry'
+
+def badge_maker name
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator names
+  names.map {|name| badge_maker(name) }
+end
+
+def assign_rooms speakers
+  speakers.each_with_index.map do |speaker, i|
+    "Hello, #{speaker}! You'll be assigned to room #{i + 1}!"
+  end
+end
+
+def printer attendees
+  batch_badge_creator(attendees).each { |badge| puts badge }
+  assign_rooms(attendees).each { |assignment| puts assignment }
+end
+# printer(["Arel", "Carol"])
